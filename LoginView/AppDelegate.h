f431793215787cc0f62d75bd52d802d71614ff87
Stanislav Sidelnikov <sidelnikov.stanislav@gmail.com>
2015-11-25 10:50:45 +0500

    Initial Commit

---
+++ LoginView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginView
+//
+//  Created by Stanislav Sidelnikov on 25/11/15.
+//  Copyright © 2015 Stanislav Sidelnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoginView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginView
+//
+//  Created by Stanislav Sidelnikov on 25/11/15.
+//  Copyright © 2015 Stanislav Sidelnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
